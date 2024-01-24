a73b0e4dcd8f3f3c4d2007b721356de94c779132
narrativityy <jake_crowley@comcast.net>
2024-01-23 23:06:41 -0800

    updated navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,28 +3,30 @@ import { FaBars, FaTimes } from 'react-icons/fa'
 import "../styles/main.css"
 
 const Navbar = () => {
-  const navRef = useRef()
+  const navRef = useRef(null)
 
   const showNavbar = () => {
     navRef.current.classList.toggle("responsive_nav")
   }
 
   return (
-    <header>
-        <h3>Jake Crowley</h3>
-        <nav ref={navRef}>
-            <a href="/#">Home</a>
-            <a href="/#">My Work</a>
-            <a href="/#">Blog</a>
-            <a href="/#">About Me</a>
-            <button className='nav-btn nav-close-btn' onClick={showNavbar}>
-                <FaTimes />
+    <div>
+        <header>
+            <h3>Jake Crowley</h3>
+            <nav ref={navRef}>
+                <a href="/#">Home</a>
+                <a href="/#">My Work</a>
+                <a href="/#">Blog</a>
+                <a href="/#">About Me</a>
+                <button className='nav-btn nav-close-btn' onClick={showNavbar}>
+                    <FaTimes />
+                </button>
+            </nav>
+            <button className='nav-btn' onClick={showNavbar}>
+                <FaBars />
             </button>
-        </nav>
-        <button className='nav-btn' onClick={showNavbar}>
-            <FaBars />
-        </button>
-    </header>
+        </header>    
+    </div>
   )
 }
 
