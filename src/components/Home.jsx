37f0961a805af1a1c241bc8bf5e73d4dbacf5474
narrativityy <jake_crowley@comcast.net>
2024-01-30 16:21:20 -0800

    redesigned navbar

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,17 +5,18 @@ const Home = () => {
   return (
     <div className="text-center mt-3">
         <div className="d-flex justify-content-center align-items-center">
-            <img id='headshot' src={headshot} height="50px" alt="Personal Portrait" />
-        </div>
-        <div className="mt-3">
-            <h3>Hello, my name is Jake</h3>
-            
+            <div className="mx-2 ms-5 text-start">
+                <h2 className="fs-1 fw-bold">Jake Crowley</h2>
+                <p className="mt-2 fw-lighter">Jake is a full-stack engineer who solves complex problems. He attended Coding Dojo and has an Associate Degree in Computer Science</p>
+            </div>
+            <img className="me-5 shadow" id='headshot' src={headshot} height="50px" alt="Personal Portrait" />
         </div>
         <div className="mt-3 d-flex justify-content-center align-items-center">
             <div className="row align-items-start">
-                <div className="col">
+                <div className="col px-5">
+                    <h3>About me</h3>
                 </div>
-                <div className="col">
+                <div className="col px-5">
                     <h3>Skills</h3>
                 </div>
             </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,22 +8,22 @@ const Navbar = () => {
   const navRef = useRef(null)
 
   const showNavbar = () => {
-    navRef.current.classList.toggle("responsive_nav")
+    navRef.current.classList.toggle("responsive_nav")``
   }
 
   return (
-    <div id='navbar-container' className='m-2 mb-0 shadow rounded'>
+    <div id='navbar-container' className='m-2 mb-0'>
         <header>
-            <img id='logo' src={logo} alt="website logo" />
+            <img className='shadow rounded' id='logo' src={logo} alt="website logo" />
             <nav ref={navRef}>
                 <Link to={"/"}>Home</Link>
                 <Link to={"/work"}>My Work</Link>
                 <Link to={"/about"} href="/#">About Me</Link>
-                <button className='nav-btn nav-close-btn' onClick={showNavbar}>
+                <button className='nav-btn nav-close-btn shadow' onClick={showNavbar}>
                     <FaTimes />
                 </button>
             </nav>
-            <button className='nav-btn' onClick={showNavbar}>
+            <button className='nav-btn shadow' onClick={showNavbar}>
                 <FaBars />
             </button>
         </header>
