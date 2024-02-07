d5422909220e278c8005bafd729693c19302a043
narrativityy <jake_crowley@comcast.net>
2024-02-06 21:49:37 -0800

    added about me content

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -25,37 +25,45 @@ const AboutMe = forwardRef((props, ref) => {
             alt="Personal Portrait"
           />
         </div>
-        <h1 className="fs-2 fw-bold text-start header">My Story</h1>
+        <h1 className="mt-4 fs-2 fw-bold text-start header">My Story</h1>
         <div className="my-4 d-flex justify-content-center align-items-center">
           <div className="row align-items-start">
             <div className="col me-1">
-              <h3>
+              <p className="fw-lighter">
                 For as long as I can remember, my life has revolved around
                 computers. From getting home from school and watching YouTube to
                 figuring out every setting I could change on my iPod, I have
-                been fascinated with tinkering with all kinds of technology.{" "}
-              </h3>
+                been fascinated with tinkering with all kinds of technology. My
+                passion had grown to the point where I wanted to build my own
+                computer, and I did just that. I spent hours researching and
+                learning about the different components and how they worked
+                together. I was so proud of myself when I finally got it to boot
+                up for the first time. I was hooked. I knew that I wanted to
+                work with computers for the rest of my life. I slowly started
+                learning about coding and Software Engineering and it igninted a
+                brand new fire in me. When I began attending college, I took my
+                first Computer Science class and I couldn't stop thinking about
+                how every piece of technology logically works.
+              </p>
             </div>
             <div className="col ms-1">
-              <h3>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-                aliquam ipsum diam, ut interdum leo vehicula at. Fusce rhoncus
-                consectetur lorem at pharetra. Suspendisse non lobortis quam.
-                Duis dui dui, rhoncus sit amet tellus ut, tempor interdum eros.
-                Nunc et vehicula dui. Aliquam auctor sollicitudin libero quis
-                semper. Suspendisse fringilla augue sit amet varius mattis.
-                Morbi nec magna ut lacus porta pretium vitae ut enim. Vivamus
-                commodo, risus et interdum finibus, libero neque feugiat urna,
-                nec volutpat sem odio sed metus. Etiam quis mi malesuada,
-                ullamcorper libero eu, dignissim sem. Donec pharetra magna
-                turpis, sit amet facilisis enim lobortis sed. Sed eu nunc in
-                tellus mattis suscipit quis id urna. Cras et tincidunt velit.
-                Duis erat lorem, dictum at neque at, maximus molestie mauris.
-                Aenean blandit est in nunc tincidunt, et porta libero dictum.
-                Orci varius natoque penatibus et magnis dis parturient montes,
-                nascetur ridiculus mus. Nunc quis leo scelerisque, feugiat felis
-                sit amet, tempor diam. Curabitur et faucibus est.
-              </h3>
+              <p className="fw-lighter">
+                By the time I graduated high school, I was a few classes short
+                of an Associate's Degree in Computer Science. I was so excited
+                to continue my education and learn more about the field.
+                However, during this time I started researching Coding Bootcamps
+                and I was intrigued. I decided to take a leap of faith and
+                enroll in a Full-Stack Web Development Bootcamp at Coding Dojo.
+                I was so excited to learn about the latest technologies and how
+                to build full-stack applications. During the bootcamp, I enjoyed
+                the team environment. I was not only able to work with other
+                students and learn from them but also able to help others and
+                learn from teaching them. This circular learning environment
+                allowed me to grow and learn at an accelerated pace. By the end
+                of the bootcamp, I had built multiple full-stack applications
+                and graduated at the top of my class, earning a Triple Black
+                Belt.
+              </p>
             </div>
           </div>
         </div>
